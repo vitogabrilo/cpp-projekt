95916fd169e9af6664212a937fad3c055f43bd33
nikomarinovic <vigxi74@gmail.com>
2026-09-25 18:43:17 +0200

    dobar kod

--- osobnipodaci.cpp
+++ osobnipodaci.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+    cout << "====================\n"
+         << "MY FIRST C++\n"
+         << "====================\n\n"
+         << "Ime: Ivan\n"
+         << "Fakultet: TVZ\n"
+         << "Godina: 1" << endl;
+    return 0;
+}
